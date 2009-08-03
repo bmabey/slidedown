5984cef3077ce23cd5bf3a4c0c32f9e5e4d555f3
Ben Mabey <ben@benmabey.com>
2009-08-03 12:05:53 -0600

    failing spec for html+erb syntax highlighting
    Note: rhtml works fine- maybe it is an issue the '+' symbol?
    I double checked and html+erb is an available lexer alias for my
    version of pygments so I don't think the problem is with my pygments
    installation.

--- spec/slide_spec.rb
+++ spec/slide_spec.rb
@@ -34,4 +34,14 @@ describe 'SlideDown' do
     MD
     Nokogiri(slide.html).at('.code.ruby').should_not be_nil
   end
+
+  it 'syntax highlights html+erb' do
+    with_markdown <<-MD
+    |@@@ html+erb
+    |  <p>Hello <%= @user.name %> </p>
+    |@@@
+    MD
+    Nokogiri(slide.html).at('.code.html+erb').should_not be_nil
+  end
+
 end
